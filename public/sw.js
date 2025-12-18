1ee09a3f467972de90793073926af64402ee3a81
SimoneItinerAI <simonesammarco01@gmail.com>
2025-12-19 00:58:10 +0100

    fix(prod): rimuovi preload splashscreen 404 e ignora schemi non http nel service worker

--- public/sw.js
+++ public/sw.js
@@ -2,11 +2,24 @@ self.addEventListener('install', (event) => {
   event.waitUntil(caches.open('itinerai-v1').then((cache) => cache.addAll(['/','/index.html'])))
 })
 self.addEventListener('fetch', (event) => {
+  const req = event.request
+  const url = req.url || ''
+  const isHttp = /^https?:/i.test(url)
+  const isGet = req.method === 'GET'
+  if (!isHttp || !isGet) {
+    event.respondWith(fetch(req).catch(() => caches.match(req)))
+    return
+  }
   event.respondWith(
-    caches.match(event.request).then((resp) => resp || fetch(event.request).then((response) => {
-      const copy = response.clone()
-      caches.open('itinerai-v1').then((cache) => cache.put(event.request, copy))
-      return response
-    }).catch(() => resp))
+    caches.match(req).then((cached) => {
+      if (cached) return cached
+      return fetch(req).then((response) => {
+        const copy = response.clone()
+        caches.open('itinerai-v1').then((cache) => {
+          try { cache.put(req, copy) } catch { /* ignore non-storable */ }
+        })
+        return response
+      }).catch(() => cached)
+    })
   )
 })
